6ff4e4830de7ef1ba15d08a872846685fc1b1da7
Losleo1211 <35976282+Losleo1211@users.noreply.github.com>
2026-09-23 20:22:50 +0200

    Leo's Messenger – V2.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 24
         targetSdk = 36
 
-        versionCode = 206
-        versionName = "2.0.6"
+        versionCode = 207
+        versionName = "2.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
